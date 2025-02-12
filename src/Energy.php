0240267d8a5d25d33ac062fcbba40b0afb9fa528
jdrazin <jonathan@drazin.net>
2025-02-12 16:15:29 +0000

    slots history

--- src/Energy.php
+++ src/Energy.php
@@ -27,7 +27,7 @@ class Energy extends Root
 
     public function slots(): string {
         $sql = 'SELECT      `n`.`slot`,
-                            TIME(`n`.`start`) AS `time`,
+                            `n`.`start` AS `datetime`,
                             ROUND(`n`.`total_load_kw`, 3),
                             ROUND(`p`.`total_load_kw`, 3) AS `previous_total_load_kw`,
                             ROUND(`n`.`grid_kw`, 3),
@@ -44,16 +44,16 @@ class Energy extends Root
                   WHERE     `n`.`slot` >= 0 AND `n`.`final`
                   ORDER BY  `n`.`slot`';
         if (!($stmt = $this->mysqli->prepare($sql)) ||
-            !$stmt->bind_result($slot, $time, $total_load_kw, $previous_total_load_kw, $grid_kw, $previous_grid_kw, $solar_kw, $previous_solar_kw) ||
+            !$stmt->bind_result($slot, $datetime, $total_load_kw, $previous_total_load_kw, $grid_kw, $previous_grid_kw, $solar_kw, $previous_solar_kw) ||
             !$stmt->execute()) {
             $message = $this->sqlErrMsg(__CLASS__, __FUNCTION__, __LINE__, $this->mysqli, $sql);
             $this->logDb('MESSAGE', $message, 'ERROR');
             throw new Exception($message);
         }
         $slots = [];
-        $slots[]     = ['slot', 'time', 'total_load_kw', 'previous_load_kw',     'grid_kw', 'previous_grid_kw', 'solar_kw', 'previous_solar_kw'];
+        $slots[]     = ['slot', 'datetime', 'total_load_kw', 'previous_load_kw',     'grid_kw', 'previous_grid_kw', 'solar_kw', 'previous_solar_kw'];
         while ($stmt->fetch()) {
-            $slots[] = [$slot,  $time,  $total_load_kw,  $previous_total_load_kw, $grid_kw, $previous_grid_kw,   $solar_kw, $previous_solar_kw];
+            $slots[] = [$slot,  $datetime,  $total_load_kw,  $previous_total_load_kw, $grid_kw, $previous_grid_kw,   $solar_kw, $previous_solar_kw];
         }
         return json_encode($slots, JSON_PRETTY_PRINT);
     }

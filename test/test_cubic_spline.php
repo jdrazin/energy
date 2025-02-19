4a2840597d1eeb85853514d58a7547dfd73fc62f
jdrazin <jonathan@drazin.net>
2025-02-19 13:07:45 +0000

    Commit

--- test/test_cubic_spline.php
+++ test/test_cubic_spline.php
@@ -1,26 +1,7 @@
 <?php //
-	namespace Energy;
-
-    error_reporting(E_ALL);
-	require_once __DIR__ . "/Battery.php";
-	
-	$unit = 'Test 10kWh battery';
-	$capacity_effective_kwh = 9.0;
-	$efficiency = 0.9;
-	$max_put_kw = 4.5;
-	$max_get_kw = 4.5;
-	$value_gbp = 7000.0;
-	
-	$battery = new Battery($unit, $capacity_effective_kwh, $efficiency, $max_put_kw, $max_get_kw, $value_gbp);
-	
-	echo 'Charge...' . PHP_EOL;
-	for ($hour=0; $hour < 24; $hour++) {
-		echo $battery->transfer_consume_j(3600*1000.0, 3600.0)['energy_j'] . PHP_EOL;
-	}
-	echo 'Discharge...' . PHP_EOL;
-	for ($hour=0; $hour < 24; $hour++) {
-		echo $battery->transfer_consume_j(-3600*1000.0, 3600.0)['energy_j'] . PHP_EOL;
-	}
+    declare(strict_types=1);
+    namespace Src;
+    require_once __DIR__ . '/../vendor/autoload.php';
 	
 	
 	

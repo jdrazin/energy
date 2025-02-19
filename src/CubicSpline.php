a57b22be0b170b9db0df34e163ec16db7a24ead1
jdrazin <jonathan@drazin.net>
2025-02-19 13:05:25 +0000

    Commit

---
+++ src/CubicSpline.php
@@ -0,0 +1,13 @@
+<?php
+namespace Src;
+
+/*
+ * wrapper to Python scipy cubic spline library
+ *
+ * see https://pythonnumericalmethods.studentorg.berkeley.edu/notebooks/chapter17.03-Cubic-Spline-Interpolation.html
+ */
+
+class CubicSpline
+{
+
+}

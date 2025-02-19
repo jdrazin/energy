9a6626b16d36166d63f448073f1e5189811c98eb
jdrazin <jonathan@drazin.net>
2025-02-19 17:43:46 +0000

    Commit

--- src/cubicSpline.py
+++ src/cubicSpline.py
@@ -13,20 +13,28 @@ multiple = int(sys.argv[index])
 # array size
 index   += 2
 size     = int(sys.argv[index])
+
+# load x, y array element pairs
+x = []
+y = []
+
+index += 2
 i = 0
-while i < number_slots:
+while i < size:
+    x.append(float(sys.argv[index]))
     index += 1
-    tariffImportPerKwhs.append(float(sys.argv[index]))
+    y.append(float(sys.argv[index]))
+    index += 1
     i+= 1
 
 # use bc_type = 'natural' adds the constraints as we described above
 f = CubicSpline(x, y, bc_type='natural')
-x_cs = np.linspace(0, 2, 10)
+x_cs = np.linspace(0, size-1, multiple)
 y_cs = f(x_cs)
 
 # output result as json
 output = {
-    "x": x_cs,
-    "y": y_cs
+    "x": x_cs.tolist(),
+    "y": y_cs.tolist()
 }
 print(json.dumps(output))

ddd8558b69814ecca7bb2d37d8f3c639a099d420
jdrazin <jonathan@drazin.net>
2025-02-26 13:16:17 +0000

    Commit

--- test/test_smtp.php
+++ test/test_smtp.php
@@ -1,8 +1,31 @@
 <?php //
 declare(strict_types=1);
 namespace Src;
+use PHPMailer\PHPMailer\Exception;
+
 require_once __DIR__ . '/../vendor/autoload.php';
-use Exception;
-use GuzzleHttp\Exception\GuzzleException;
-	
-	
+
+$credentials = [
+                    'host'      =>  'smtp-relay.brevo.com',
+                    'port'      =>  587,
+                    'user'      =>  '86b611001@smtp-brevo.com',
+                    'password'  =>  'YzS12IFnJfsK0EG6',
+                    'security'  =>  'tls',
+                    'from'      =>  ['email' => 'jonathan@drazin.net', 'name' => 'Jonathan Drazin'],
+                    'to'        => 	['email' => 'jonathan@drazin.net', 'name' => 'Jonathan Drazin'],
+                    'bcc'       =>  [],
+                    'reply'     =>  [],
+                ];
+
+$content =  [
+            'subject'   =>  "Test Subject",
+            'body'       => "Test Body",
+            ];
+
+try {
+  (new SMTPEmail())->email($credentials, $content);       // traverse all tariffs
+  exit(0);
+}
+catch (Exception $e) {
+
+}

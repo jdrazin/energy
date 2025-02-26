498e833d3b1a8fc2072a03a9368f37d1e0a60d71
jdrazin <jonathan@drazin.net>
2025-02-26 13:23:30 +0000

    Commit

--- src/SMTPEmail.php
+++ src/SMTPEmail.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Src;
-use Credentials;
-use PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -13,29 +13,40 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 class SMTPEmail
 {
-    public function email()
+    /**
+     * @throws Exception
+     */
+    public function email($credentials, $content): void
     {
         $mail = new PHPMailer();
-        $mail->SMTPDebug = 3;                                 // Enable verbose debug output
+        $mail->SMTPDebug = 3;                             // Enable verbose debug output
         $mail->Debugoutput = 'html';
-        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->isSMTP();                                  // Set mailer to use SMTP
         $mail->SMTPOptions = ['ssl' => ['verify_peer' => false,
             'verify_peer_name' => false,
             'allow_self_signed' => true]];
-        $mail->Host = Credentials::E_HOST;       // Specify main and backup SMTP servers
-        $mail->Username = Credentials::E_USERNAME;   // SMTP username
-        $mail->Password = Credentials::E_PASSWORD;   // SMTP password
-        $mail->SMTPAuth = true;                      // Enable SMTP authentication
-        $mail->SMTPSecure = 'tls';                     // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;                       // TCP port to connect to
-        $mail->setFrom('info@shopsee.co.uk', 'info@shopsee.co.uk');
-        $mail->addAddress('jonathan@drazin.net', 'Jonathan Drazin');     // Add a recipient
-        $mail->addReplyTo('info@shopsee.co.uk', 'info@shopsee.co.uk');
-        $mail->addBCC('info@shopsee.co.uk');
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Host         = $credentials['host'];       // Specify main and backup SMTP servers
+        $mail->Port         = $credentials['host'];       // TCP port to connect to
+        $mail->Username     = $credentials['user'];       // SMTP username
+        $mail->Password     = $credentials['password'];   // SMTP password
+        $mail->SMTPAuth     = true;                       // enable SMTP authentication
+        $mail->SMTPSecure   = $credentials['security'];   // security method
+        if ($from = $credentials['from']) {
+            $mail->setFrom($from['email'], $from['name']);
+        }
+        if ($to    = $credentials['to'])    {
+            $mail->addAddress($to['email'], $to['name']);
+        }
+        if ($bcc   = $credentials['bcc'])   {
+            $mail->addBCC($bcc['email']);
+        }
+        if ($reply = $credentials['reply']) {
+            $mail->addReplyTo($reply['email']);
+        }
+        $mail->isHTML($content['html'] ?? false);
+        $mail->Subject = $content['subject']  ?? '';
+        $mail->Body    = $content['bodyHTML'] ?? '';
+        $mail->AltBody = $content['body']     ?? '';
         if (!$mail->send()) {
             echo 'Message could not be sent';
         } else {

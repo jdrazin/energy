b93f814e6a2269d781363f77fc9a595bb11efc0e
jdrazin <jonathan@drazin.net>
2025-02-26 15:24:39 +0000

    Commit

--- src/SMTPEmail.php
+++ src/SMTPEmail.php
@@ -3,6 +3,7 @@
 namespace Src;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -14,43 +15,76 @@ require_once __DIR__ . '/../vendor/autoload.php';
 class SMTPEmail
 {
     /**
-     * @throws Exception
      */
-    public function email($credentials, $content): void
-    {
+    public function email($credentials, $content): void {
         $mail = new PHPMailer();
-        $mail->SMTPDebug = 3;                             // Enable verbose debug output
-        $mail->Debugoutput = 'html';
-        $mail->isSMTP();                                  // Set mailer to use SMTP
-        $mail->SMTPOptions = ['ssl' => ['verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true]];
-        $mail->Host         = $credentials['host'];       // Specify main and backup SMTP servers
-        $mail->Port         = $credentials['host'];       // TCP port to connect to
-        $mail->Username     = $credentials['user'];       // SMTP username
-        $mail->Password     = $credentials['password'];   // SMTP password
-        $mail->SMTPAuth     = true;                       // enable SMTP authentication
-        $mail->SMTPSecure   = $credentials['security'];   // security method
-        if ($from = $credentials['from']) {
-            $mail->setFrom($from['email'], $from['name']);
-        }
-        if ($to    = $credentials['to'])    {
-            $mail->addAddress($to['email'], $to['name']);
-        }
-        if ($bcc   = $credentials['bcc'])   {
-            $mail->addBCC($bcc['email']);
-        }
-        if ($reply = $credentials['reply']) {
-            $mail->addReplyTo($reply['email']);
-        }
-        $mail->isHTML($content['html'] ?? false);
-        $mail->Subject = $content['subject']  ?? '';
-        $mail->Body    = $content['bodyHTML'] ?? '';
-        $mail->AltBody = $content['body']     ?? '';
-        if (!$mail->send()) {
-            echo 'Message could not be sent';
-        } else {
+        try {
+
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'visionsrenewable@gmail.com';
+            $mail->Password = 'mQ9ZLsrSAeSlAIwhLYP1';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+            $mail->setFrom('visionsrenewable@gmail.com', 'V R');
+            $mail->addAddress('jonathan@drazin.net', 'J D');
+            $mail->isHTML(false);
+            $mail->Subject  = 'Test subject';
+            $mail->Body     = 'Test body';
+            $mail->AltBody  = 'Test alt body';
+            $mail->send();
+
+
+
+
+
+
+
+
+
+
+
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;            // Enable verbose debug output
+            $mail->isSMTP();                                  // Set mailer to use SMTP
+            $mail->SMTPOptions = [
+                                        'ssl' => [
+                                                'verify_peer'       => false,
+                                                'verify_peer_name'  => false,
+                                                'allow_self_signed' => true
+                                                ]
+                                  ];
+            $mail->Host         = $credentials['host'];             // Specify main and backup SMTP servers
+            $mail->SMTPAuth     = true;                             // enable SMTP authentication
+            $mail->Username     = $credentials['user'];             // SMTP username
+            $mail->Password     = $credentials['password'];         // SMTP password
+            $mail->SMTPSecure   = PHPMailer::ENCRYPTION_SMTPS;      // security method
+            $mail->Port         = $credentials['port'];             // TCP port to connect to
+            if ($from = $credentials['from']) {
+                $mail->setFrom($from['email'], $from['name']);
+            }
+            if ($to = $credentials['to']) {
+                $mail->addAddress($to['email'], $to['name']);
+            }
+            if ($cc = $credentials['cc']) {
+                $mail->addCC($cc['email']);
+            }
+            if ($bcc = $credentials['bcc']) {
+                $mail->addBCC($bcc['email']);
+            }
+            if ($reply = $credentials['reply']) {
+                $mail->addReplyTo($reply['email']);
+            }
+            $mail->isHTML($content['html'] ?? false);
+            $mail->Subject  = $content['subject']   ?? '';
+            $mail->Body     = $content['bodyHTML']  ?? '';
+            $mail->AltBody  = $content['bodyAlt']   ?? '';
+            $mail->send();
             echo 'Message has been sent';
+    }
+    catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
         }
     }
 }

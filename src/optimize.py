7c32430028af4040ea29e2270fe6077899bb633a
jdrazin <jonathan@drazin.net>
2025-03-04 17:28:31 +0000

    Commit

--- src/optimize.py
+++ src/optimize.py
@@ -8,29 +8,26 @@ from scipy.optimize import minimize
 
 # define energy cost
 def day_cost(grid_kws):
-    cost_energy_average_per_kwh_acc = 0.0                           # accumulator for calculating average energy cost
-    battery_level_kwh               = batteryEnergyInitialKwh       # initial battery level
-    normalisation_battery_energy_coefficient = 12.0 / (1.0 + (11.0 * batteryWearConstantCoefficient) + (24.0 * batteryWearOutOfSpecCoefficient * batteryWearOutOfSpecActivationEnergyKwh / batteryCapacityKwh))
-    normalisation_inverter_power_coefficient = 12.0 / (1.0 + (24.0 * batteryWearOutOfSpecActivationEnergyKwh * batteryWearOutOfSpecCoefficient / (batteryMaxDischargeRateKw + batteryMaxChargeRateKw)))
-    cost_grid_import                = 0.0
-    cost_grid_export                = 0.0
-    cost_energy_wear_out_of_spec    = 0.0
-    cost_power_out_of_spec          = 0.0
-    import_kwh                      = 0.0
-    export_kwh                      = 0.0
-    slot_count                      = 0
+    cost_energy_average_per_kwh_acc  = 0.0                           # accumulator for calculating average energy cost
+    battery_level_kwh                = batteryEnergyInitialKwh       # initial battery level
+    normalisation_energy_coefficient = 12.0 / (1.0 + (11.0 * batteryWearConstantCoefficient) + (24.0 * batteryWearOutOfSpecCoefficient * batteryWearOutOfSpecActivationEnergyKwh / batteryCapacityKwh))
+    normalisation_power_coefficient  = 12.0 / (1.0 + (24.0 * batteryWearOutOfSpecActivationEnergyKwh * batteryWearOutOfSpecCoefficient / (batteryMaxDischargeRateKw + batteryMaxChargeRateKw)))
+    cost_grid_import                 = 0.0
+    cost_grid_export                 = 0.0
+    cost_grid_out_of_spec            = 0.0
+    cost_energy_wear_out_of_spec     = 0.0
+    cost_power_out_of_spec           = 0.0
+    import_kwh                       = 0.0
+    export_kwh                       = 0.0
+    slot_count                       = 0
     while slot_count < number_slots:
         grid_power_slot_kw = grid_kws[slot_count]
-        if grid_power_slot_kw > exportLimitKw:              # clip grid power to import/export limit
-            grid_power_slot_kw = exportLimitKw
-        else:
-            if grid_power_slot_kw < -importLimitKw:
-                grid_power_slot_kw = -importLimitKw
-        load_kw               = total_load_kws[slot_count]
+
+        total_load_kw         = total_load_kws[slot_count]
         tariff_import_per_kwh = tariffImportPerKwhs[slot_count]
         tariff_export_per_kwh = tariffExportPerKwhs[slot_count]
         energy_grid_kwh       = grid_power_slot_kw * slotDurationHour
-        total_load_kwh              = load_kw * slotDurationHour
+        total_load_kwh              = total_load_kw * slotDurationHour
         #
         # grid
         if energy_grid_kwh > 0.0:
@@ -40,9 +37,19 @@ def day_cost(grid_kws):
             import_kwh       += -energy_grid_kwh
             cost_grid_import -= tariff_import_per_kwh * energy_grid_kwh
 
+        # grid out of spec import / export limit
+        cost_grid_out_of_spec += wear_out_of_spec_cost( grid_power_slot_kw,
+                                                       -importLimitKw,
+                                                        exportLimitKw,
+                                                        0.0,
+                                                        0.0,
+                                                        batteryWearOutOfSpecCoefficient,
+                                                        batteryWearOutOfSpecActivationEnergyKwh,
+                                                        normalisation_power_coefficient) * slotDurationHour
+
         # battery
         battery_charge_kwh   = -energy_grid_kwh - total_load_kwh
-        battery_charge_kw    = -grid_power_slot_kw - load_kw
+        battery_charge_kw    = -grid_power_slot_kw - total_load_kw
         battery_level_kwh   += battery_charge_kwh * batteryOneWayStorageEfficiency
 
         # operational and out of spec wear
@@ -53,7 +60,7 @@ def day_cost(grid_kws):
                                                                 batteryWearConstantCoefficient,
                                                                 batteryWearOutOfSpecCoefficient,
                                                                 batteryWearOutOfSpecActivationEnergyKwh,
-                                                                normalisation_battery_energy_coefficient) * slotDurationHour
+                                                                normalisation_energy_coefficient) * slotDurationHour
 
         # out of current spec
         cost_power_out_of_spec += wear_out_of_spec_cost(       battery_charge_kw,
@@ -63,13 +70,12 @@ def day_cost(grid_kws):
                                                 0.0,
                                                                 batteryWearOutOfSpecCoefficient,
                                                                 batteryWearOutOfSpecActivationEnergyKwh,
-                                                                normalisation_inverter_power_coefficient) * slotDurationHour
+                                                                normalisation_power_coefficient) * slotDurationHour
 
         cost_energy_average_per_kwh_acc += 0.5 * (tariff_import_per_kwh + tariff_export_per_kwh) # accumulate average energy cost
         slot_count += 1
     cost_level_change = (batteryEnergyInitialKwh - battery_level_kwh) * cost_energy_average_per_kwh_acc / number_slots
-    cost = cost_grid_import + cost_grid_export + cost_energy_wear_out_of_spec + cost_power_out_of_spec + cost_level_change
-    return cost
+    return cost_grid_import + cost_grid_export + cost_grid_out_of_spec + cost_energy_wear_out_of_spec + cost_power_out_of_spec + cost_level_change
 
 # define wear function
 def wear_out_of_spec_cost(x, x_min, x_max, wear_cost_average, constant_coefficient, out_of_spec_coefficient, activation, normalisation_coefficient):

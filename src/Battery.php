275de3dfcbc2b467336f57b1ccad1bfe04d7630d
jdrazin <jonathan@drazin.net>
2025-03-14 15:28:55 +0000

    Commit

--- src/Battery.php
+++ src/Battery.php
@@ -7,15 +7,14 @@ require_once __DIR__ . "/Energy.php";
 
 class Battery extends Component
 {
-    public float    $max_charge_w, $store_j, $efficiency, $initial_raw_capacity_kwh, $cycles_to_reduced_capacity,
+    public float    $max_charge_w, $store_j, $one_way_efficiency, $initial_raw_capacity_kwh, $cycles_to_reduced_capacity,
                     $reduced_capacity, $capacity_kwh, $store_j_max, $max_discharge_w, $cycles;
-    public Inverter $inverter;
 
     public function __construct($component, $time, $npv)
     {
         parent::__construct($component, $time, $npv);
         if ($this->active) {
-            $this->efficiency                   = $component['inverter']['one_way_storage_efficiency'] ?? 1.0;
+            $this->one_way_efficiency           = sqrt(($component['round_trip_efficiency_percent'] ?? 100.0) / 100.0);
             $this->initial_raw_capacity_kwh     = $component['initial_raw_capacity_kwh'] ?? 0.0;
             $this->cycles_to_reduced_capacity   = $component['projection']['cycles_to_reduced_capacity'] ?? 1E9;
             $this->reduced_capacity             = 1.0 - (($component['projection']['reduced_capacity_percent'] ?? 0)/100.0);
@@ -23,7 +22,6 @@ class Battery extends Component
             $this->max_discharge_w              = 1000.0 * $component['max_discharge_kw'];
             $this->store_j                      = 0.0;
             $this->store_j_max                  = $this->initial_raw_capacity_kwh * Energy::JOULES_PER_KWH;
-            $this->inverter                     = new Inverter($component['inverter'] ?? null, $time, $npv);
             $this->cycles                       = 0.0;
         }
     }
@@ -33,11 +31,9 @@ class Battery extends Component
         if ($request_consumed_j > 0.0) {  // charge
             $request_consumed_j = min($request_consumed_j, $this->max_charge_w * $this->step_s);
             if ($this->store_j < $this->store_j_max) {                // only charge when battery not full
-                $transfer_inverter = $this->inverter->transfer_consume_j($request_consumed_j * $this->efficiency, false);
-                $transferred = $transfer_inverter['transfer'];
-                $this->store_j += $transferred;
+                $this->store_j += ($request_consumed_j * $this->one_way_efficiency);
                 $this->age($request_consumed_j);
-                return ['transfer' => $transferred,
+                return ['transfer' => $request_consumed_j,
                         'consume'  => $request_consumed_j];
             } else {
                 return ['transfer' => 0.0,                            // no charge, battery is full
@@ -46,9 +42,7 @@ class Battery extends Component
         } else {                      // discharge
             if ($this->store_j > 0.0) {
                 $request_consumed_j = max($request_consumed_j, -$this->max_discharge_w * $this->step_s);
-                $transfer_inverter = $this->inverter->transfer_consume_j(-$request_consumed_j, true);
-                $transferred = $transfer_inverter['consume'];
-                $this->store_j -= ($transferred / $this->efficiency);
+                $this->store_j += ($request_consumed_j / $this->one_way_efficiency);
                 $this->age($request_consumed_j);
                 return ['transfer' => $request_consumed_j,
                         'consume'  => 0.0];

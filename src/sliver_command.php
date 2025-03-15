d5bdff2045786ce0b2915eb0066ce926601553ed
jdrazin <jonathan@drazin.net>
2025-03-15 14:09:51 +0000

    lots history

--- src/sliver_command.php
+++ src/sliver_command.php
@@ -23,26 +23,34 @@ try {
     $slivers                = new Sliver();
     $givenergy              = new GivEnergy();
     $energy_cost            = new EnergyCost(null, null);
+    $energy_cost->makeNormalisationEnergyCoefficient();
+    $energy_cost->makeNormalisationPowerCoefficient();
     $slot_target_parameters = $slivers->slotTargetParameters();             // get slot target parameters
-    $battery                = $givenergy->latest()['data']['battery'];      // get battery data
-    $battery_level_kwh      = $battery['percent']*$energy_cost->batteryCapacityKwh/100.0;
+    $battery                = $givenergy->latest();      // get battery data
+    $battery_level_kwh      = $battery['battery']['percent']*$energy_cost->batteryCapacityKwh/100.0;
     $net_load_kw            = ($battery['consumption']-$battery['solar']['power'])/1000.0;
     $charge_power_min_kw    = -$givenergy->battery['max_discharge_kw'];
     $charge_power_max_kw    =  $givenergy->battery['max_charge_kw'];
     $charge_power_increment_kw = ($charge_power_max_kw - $charge_power_min_kw)/CHARGE_POWER_LEVELS;
-    $levels = [];
     $duration_hour = SLIVER_DURATION_MINUTES / 60.0;
-    for ($charge_power_kw = $charge_power_min_kw; $charge_power_min_kw <= $charge_power_max_kw; $charge_power_kw += $charge_power_increment_kw) {
-        $grid_power_kw            = $net_load_kw + $charge_power_kw;
-        $grid_cost_per_kwh        = $grid_power_kw < 0.0 ? $slot_target_parameters['import_gbp_per_kwh'] : $slot_target_parameters['export_gbp_per_kwh'];
-        $cost_wear_per_charge_kwh = $energy_cost->costWearOutOfSpecGbp($grid_power_kw, $charge_power_kw, $battery_level_kwh, $duration_hour);
-        $levels[] = [];
+    $charge_power_kw = $charge_power_min_kw;
+    $optimum_cost_per_kwh = null;
+    for ($level = 0; $level <= CHARGE_POWER_LEVELS; $level++) {
+        $grid_power_kw                      = $net_load_kw + $charge_power_kw;
+        $cost_grid_per_kwh                  = $grid_power_kw < 0.0 ? $slot_target_parameters['import_gbp_per_kwh'] : $slot_target_parameters['export_gbp_per_kwh'];
+        $cost_wear_out_of_spec_gbp_per_kwh  = $energy_cost->costWearOutOfSpecGbp($grid_power_kw, $charge_power_kw, $battery_level_kwh, $duration_hour);
+        $cost_per_kwh                       = $cost_grid_per_kwh + $cost_wear_out_of_spec_gbp_per_kwh;
+        if (is_null($optimum_cost_per_kwh)) {
+            $optimum_charge_power_kw = $charge_power_kw;
+            $optimum_cost_per_kwh    = $cost_per_kwh;
+        }
+        if ($cost_per_kwh < $optimum_cost_per_kwh) {
+            $optimum_charge_power_kw = $charge_power_kw;
+            $optimum_cost_per_kwh    = $cost_per_kwh;
+        }
+        $charge_power_kw += $charge_power_increment_kw;
     }
-
-
-
-
-
+    $c = $optimum_charge_power_kw;
 }
 catch (Exception $e) {
     echo $e->getMessage();

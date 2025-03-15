a066dea659f920a9badadf7321b7b0d3bdd64121
jdrazin <jonathan@drazin.net>
2025-03-15 17:58:35 +0000

    lots history

--- src/Sliver.php
+++ src/Sliver.php
@@ -60,24 +60,27 @@ class Sliver extends Root
      * @throws Exception
      */
     public function optimum_charge_w(): int {
-        $givenergy              = new GivEnergy();
-        $energy_cost            = new EnergyCost(null, null);
+        $givenergy                  = new GivEnergy();
+        $energy_cost                = new EnergyCost(null, null);
         $energy_cost->makeNormalisationEnergyCoefficient();
         $energy_cost->makeNormalisationPowerCoefficient();
-        $slot_target_parameters =  $this->slotTargetParameters();             // get slot target parameters
-        $battery                =  $givenergy->latest();      // get battery data
-        $battery_level_percent  =  $battery['battery']['percent'];
-        $battery_level_kwh      =  $battery_level_percent*$energy_cost->batteryCapacityKwh/100.0;
-        $house_load_kw          =  $battery['consumption']/1000.0;
-        $solar_kw               =  $battery['solar']['power']/1000.0;
-        $net_load_kw            =  $house_load_kw-$solar_kw;
-        $charge_min_kw          = -$givenergy->battery['max_discharge_kw'];
-        $charge_max_kw          =  $givenergy->battery['max_charge_kw'];
-        $charge_increment_kw    = ($charge_max_kw - $charge_min_kw)/self::CHARGE_POWER_LEVELS;
-        $duration_hour          = self::SLIVER_DURATION_MINUTES / 60.0;
-        $charge_kw              = $charge_min_kw;
-        $optimum_cost_per_hour  = null;
-        $optimum_charge_kw      = null;
+        $slot_target_parameters     =  $this->slotTargetParameters();             // get slot target parameters
+        $slot_mode                  = $slot_target_parameters['mode'];
+        $slot_abs_charge_power_w    = $slot_target_parameters['abs_charge_power_w'];
+        $slot_target_level_percent  = $slot_target_parameters['target_level_percent'];
+        $battery                    =  $givenergy->latest();      // get battery data
+        $battery_level_percent      =  $battery['battery']['percent'];
+        $battery_level_kwh          =  $battery_level_percent*$energy_cost->batteryCapacityKwh/100.0;
+        $house_load_kw              =  $battery['consumption']/1000.0;
+        $solar_kw                   =  $battery['solar']['power']/1000.0;
+        $net_load_kw                =  $house_load_kw-$solar_kw;
+        $charge_min_kw              = -$givenergy->battery['max_discharge_kw'];
+        $charge_max_kw              =  $givenergy->battery['max_charge_kw'];
+        $charge_increment_kw        = ($charge_max_kw - $charge_min_kw)/self::CHARGE_POWER_LEVELS;
+        $duration_hour              = self::SLIVER_DURATION_MINUTES / 60.0;
+        $charge_kw                  = $charge_min_kw;
+        $optimum_cost_per_hour      = null;
+        $optimum_charge_kw          = null;
         for ($level = 0; $level <= self::CHARGE_POWER_LEVELS; $level++) {
             $grid_power_kw                      = $net_load_kw + $charge_kw;
             $cost_grid_per_hour                 = ($grid_power_kw < 0.0 ? $slot_target_parameters['import_gbp_per_kwh'] : $slot_target_parameters['export_gbp_per_kwh'])*$charge_kw;
@@ -93,10 +96,10 @@ class Sliver extends Root
             }
             $charge_kw += $charge_increment_kw;
         }
-        $sql = 'INSERT INTO `slivers`  (`charge_w`, `level_percent`, `slot_mode`, `slot_abs_charge_power_w`, `slot_target_level_percent`, `house_load_kw`, `solar_kw`) 
-                                VALUES ($charge_kw,  $level_percent, $slot_mode,  $slot_abs_charge_power_w,  $slot_target_level_percent,  $house_load_kw,  $solar_kw)';
+        $sql = 'INSERT INTO `slivers`  (`charge_kw`,    `level_percent`,    `slot_mode`,    `slot_abs_charge_power_w`,  `slot_target_level_percent`,    `house_load_kw`,    `solar_kw`) 
+                                VALUES (?,              ?,                  ?,              ?,                          ?,                              ?,                  ?)';
         if (!($stmt = $this->mysqli->prepare($sql)) ||
-            !$stmt->bind_param('disdidd', $charge_kw,  $level_percent,  $slot_mode,  $slot_abs_charge_power_w,   $slot_target_level_percent, $house_load_kw,  $solar_kw) ||
+            !$stmt->bind_param('disdidd', $charge_kw,$battery_level_percent, $slot_mode, $slot_abs_charge_power_w, $slot_target_level_percent, $house_load_kw, $solar_kw) ||
             !$stmt->execute() ||
             !$this->mysqli->commit()) {
             $message = $this->sqlErrMsg(__CLASS__, __FUNCTION__, __LINE__, $this->mysqli, $sql);

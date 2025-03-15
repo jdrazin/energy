9ba2f785d1cb11c5cf465880bbbf8662c6825cee
jdrazin <jonathan@drazin.net>
2025-03-15 08:36:13 +0000

    lots history

--- src/sliver_command.php
+++ src/sliver_command.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 namespace Src;
 use Exception;
 use GuzzleHttp\Exception\GuzzleException;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
+/*
+ * "slither" is single battery charging control command, sent multiple times during each time slot.
+ *
+ */
 
 error_reporting(E_ALL);
 set_time_limit(36000);
@@ -14,10 +17,11 @@ ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
 try {
-    $slivers = new Slivers();
-    $sliver = $slivers->finalSlotZeroParameters();
+    $slivers = new Sliver();
     $givenergy = new GivEnergy();
-    $battery_now = $givenergy->batteryNow();
+    $slot_target_parameters = $slivers->slotTargetParameters();        // get slither target parameters
+    $battery_now = $givenergy->batteryNow();                         // get current battery charge state
+    // get instantaneous net_load = house_load - solar_generation
 
 }
 catch (Exception $e) {

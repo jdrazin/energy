1e0ab3f7cd72e6943965562a12230b7be3314060
jdrazin <jonathan@drazin.net>
2025-03-16 12:53:01 +0000

    Commit

--- src/sliver_command.php
+++ src/sliver_command.php
@@ -15,8 +15,34 @@ ini_set('mysql.connect_timeout', '36000');
 ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
+const   PID_FOLDER                          = '/var/www/html/energy/',
+        USE_PID_SEMAPHORE                   = false,
+        USE_CRONTAB                         = false,
+        ARGS                                = ['CRON' => 1],
+        INITIALISE_ON_EXCEPTION             = true,
+        EMAIL_NOTIFICATION_ON_ERROR         = true,
+        ENABLE_SLOT_COMMANDS                = false,
+        ACTIVE_TARIFF_COMBINATION_ONLY      = false;
+
 try {
-    echo (new Sliver())->optimum_charge_w();
+    $pid_filename = PID_FOLDER . basename(__FILE__, '.php') . '.pid';
+    if (USE_PID_SEMAPHORE) {
+        if (file_exists($pid_filename)) {
+            echo 'Cannot start: semaphore exists';
+            exit(1);
+        }
+        else {
+            file_put_contents($pid_filename, getmypid());
+        }
+    }
+    if ((($cron = (strtolower(trim($argv[ARGS['CRON']] ?? '')) == 'cron')) && USE_CRONTAB) || !$cron) {
+        echo (new Sliver())->optimum_charge_w();
+    }
+    if (USE_PID_SEMAPHORE) {
+        if (!unlink($pid_filename)) {
+            throw new Exception('Cannot delete semaphore');
+        }
+    }
 }
 catch (Exception $e) {
     echo $e->getMessage();

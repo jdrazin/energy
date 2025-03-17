46734660ecdd9eff7cdad86e70c1c0d6226951a4
jdrazin <jonathan@drazin.net>
2025-03-17 18:41:47 +0000

    Commit

--- src/sliver_handler.php
+++ src/sliver_handler.php
@@ -17,7 +17,7 @@ ini_set('mysql.connect_timeout','36000');
 
 const   PID_FOLDER                          = '/var/www/html/energy/',
         USE_PID_SEMAPHORE                   = false,
-        USE_CRONTAB                         = true,
+        USE_CRONTAB                         = false,
         ARGS                                = ['CRON' => 1],
         INITIALISE_ON_EXCEPTION             = false,
         EMAIL_NOTIFICATION_ON_ERROR         = false;

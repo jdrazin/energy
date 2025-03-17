0fad001333a0f86d5955dbc3cbec6edb83cecb22
jdrazin <jonathan@drazin.net>
2025-03-17 09:44:54 +0000

    Commit

--- src/DbSlots.php
+++ src/DbSlots.php
@@ -17,7 +17,8 @@ class DbSlots extends Root
     public function __construct()
     {
         parent::__construct();
-        $sql = 'DELETE FROM `slots`';
+        $sql = 'DELETE FROM `slots` 
+                    WHERE NOT `final`';
         if (!($stmt = $this->mysqli->prepare($sql)) ||
             !$stmt->execute()) {
             $message = $this->sqlErrMsg(__CLASS__, __FUNCTION__, __LINE__, $this->mysqli, $sql);

8ea03ac27e88233facb731d44750ef3e65bde2de
jdrazin <jonathan@drazin.net>
2025-03-17 17:32:53 +0000

    Commit

--- test/test_wear.php
+++ test/test_wear.php
@@ -13,13 +13,7 @@ ini_set('mysql.connect_timeout', '36000');
 ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
-const  GRID_KW              = 6.0,
-       CHARGE_KW            = 3.0,
-       BATTERY_LEVEL_KWH    = 9.0,
-       DURATION_HOUR        = 1.0/60.0;
-
 $energy_cost = new EnergyCost(null, null);
-$wear_gbp_per_hour = $energy_cost->wearGbpPerHour(GRID_KW, CHARGE_KW, BATTERY_LEVEL_KWH, DURATION_HOUR);
 exit(0);
 
 

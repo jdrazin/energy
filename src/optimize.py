21977ca672c8810f649a333e497daa9bb4e22d47
jdrazin <jonathan@drazin.net>
2025-04-09 13:17:42 +0100

    Commit

--- src/optimize.py
+++ src/optimize.py
@@ -51,9 +51,9 @@ def dayCostGbp(X):
             grid_limited_kw = grid_unlimited_kw
 
         # tie export to battery discharge limit when no net solar
-  #      if solar_clipped_kw - load_house_kw < 0.0:
-  #          if grid_limited_kw > batteryMaxDischargeRateKw:
-  #              grid_limited_kw = batteryMaxDischargeRateKw
+        if solar_clipped_kw - load_house_kw < 0.0:
+            if grid_limited_kw > batteryMaxDischargeRateKw:
+                grid_limited_kw = batteryMaxDischargeRateKw
 
         grid_kwh              = grid_limited_kw   * slotDurationHour
         battery_charge_kwh    = battery_charge_kw * slotDurationHour

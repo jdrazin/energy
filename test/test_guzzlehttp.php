3a8a81ca83ac3b6501a2452c9ac7ebda3e3cf7e5
jdrazin <jonathan@drazin.net>
2025-04-22 17:41:47 +0100

    ?

--- test/test_guzzlehttp.php
+++ test/test_guzzlehttp.php
@@ -3,15 +3,21 @@
     use GuzzleHttp\Client;
     use GuzzleHttp\Exception\GuzzleException;
 
+    require_once __DIR__ . '/../vendor/autoload.php';
+
     $url = 'https://www.drazin.net:8443/control/slot_solution?token=%3EM%7C%24-u%5Bdg%22rZssv%7Cf%7CkO~D%2CPHBer0%23%5D~';
     $headers = [
         'Content-Type' => 'application/json',
         'Accept' => 'application/json'
     ];
     $query = [];
-    $client = new Client;;
     try {
+        $client = new Client();
         $response = $client->get($url, ['headers' => $headers, 'query' => $query]);
-    } catch (GuzzleException $e) {
+    }
+    catch (GuzzleHttp\Exception\ClientException $e) {
+        $a = 1;
+    }
+    catch (GuzzleException $e) {
         $a = 1;
     }

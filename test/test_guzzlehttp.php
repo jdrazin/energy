e122ea7768b510719c47e1d767571e9f271309d0
jdrazin <jonathan@drazin.net>
2025-04-22 13:15:52 +0100

    ?

--- test/test_guzzlehttp.php
+++ test/test_guzzlehttp.php
@@ -1,32 +1,17 @@
 <?php
-	namespace Energy;
+	namespace Test;
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Exception\GuzzleException;
 
-    error_reporting(E_ALL);
-	require_once __DIR__ . '/ThermalTank.php';
-	
-	$unit = 'Test 10kWh battery';
-	$volume_m3 = 0.2;
-	$half_life_days = 1.0;
-	$value_gbp = 1000.0;
-	$temperature_ambient_c = 20.0;
-	
-	$hot_water_tank = new ThermalTank($unit, $volume_m3, $half_life_days, $temperature_ambient_c, $value_gbp);
-	
-	echo 'Charge...' . PHP_EOL;
-	for ($hour=0; $hour < 24; $hour++) {
-		$charge = $hot_water_tank->transfer_consume_j(3600*1000.0,
-										  3600.0,
-			                              $temperature_ambient_c,
-										  null,
-			                              false);
-		echo $charge['energy_j'] . PHP_EOL;
-	}
-	echo 'Discharge...' . PHP_EOL;
-	for ($hour=0; $hour < 24; $hour++) {
-		$charge = $hot_water_tank->transfer_consume_j(-3600*1000.0,
-			                              $temperature_ambient_c,
-										  null,
-			                              false);
-		echo $charge['energy_j'] . PHP_EOL;
-	}
-	
+    $url = 'https://www.drazin.net:8443/control/slot_solution?token=%3EM%7C%24-u%5Bdg%22rZssv%7Cf%7CkO~D%2CPHBer0%23%5D~';
+    $headers = [
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+    ];
+    $query = [];
+    $client = new Client;;
+    try {
+        $response = $client->get($url, ['headers' => $headers, 'query' => $query]);
+    } catch (GuzzleException $e) {
+        $a = 1;
+    }

7a492467f680487da8ffc1b43be0badd39e5936c
jdrazin <jonathan@drazin.net>
2025-06-05 18:05:37 +0100

    Corrected

--- src/slot_solver.php
+++ src/slot_solver.php
@@ -42,7 +42,7 @@ ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
 const PID_FOLDER                     = '/var/www/html/energy/pids/',
-      DEBUG                          = true,  // disable cron and semaphore single thread control
+      DEBUG                          = false,  // disable cron and semaphore single thread control
       DEBUG_MINIMISER                = false,
       ARGS                           = ['CRON' => 1],
       INITIALISE_ON_EXCEPTION        = false,

e3daa6c18f7a9f628dcbbc3cc9da64e0c0445c1d
jdrazin <jonathan@drazin.net>
2025-06-16 13:26:56 +0100

    Corrected

--- src/slice_solver.php
+++ src/slice_solver.php
@@ -16,13 +16,13 @@ ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
 const   PID_FOLDER                  = '/var/www/html/energy/pids/',
-        DEBUG                       = true,         // disable cron and semaphore single thread control
+        DEBUG                       = false,         // disable cron and semaphore single thread control
         DEBUG_MINIMISER             = false,
         DEBUG_MINIMISER_USE_FAIL    = false,        // otherwise use last OK
         ARGS                        = ['CRON' => 1],
         INITIALISE_ON_EXCEPTION     = false,
         EMAIL_NOTIFICATION_ON_ERROR = false,
-        GIVENERGY_ENABLE            = false;
+        GIVENERGY_ENABLE            = true;
 
 try {
     if (!DEBUG) {

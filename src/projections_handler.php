342b6e01045bd4ce7e9c52efbc442f4a704fa1ae
jdrazin <jonathan@drazin.net>
2025-06-28 11:06:10 +0000

    lots history

--- src/projections_handler.php
+++ src/projections_handler.php
@@ -9,8 +9,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 error_reporting(E_ALL);
 set_time_limit(36000);
 ini_set('mysql.connect_timeout', '36000');
-ini_set('max_execution_time', '36000');
-ini_set('mysql.connect_timeout','36000');
+ini_set('max_execution_time',    '36000');
+ini_set('mysql.connect_timeout', '36000');
 
 const     DEBUG                             = false,
           FOLDER_PID                        = '/var/www/html/energy/pids/',

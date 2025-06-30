6cb82c07a636c0d2260ebd5e9c0de0911602bf6a
jdrazin <jonathan@drazin.net>
2025-06-30 12:11:32 +0000

    lots history

--- src/Component.php
+++ src/Component.php
@@ -24,32 +24,23 @@ class Component extends Root // extends Root
         }
     }
 
-    public function value_maintenance($time): void
-    {
-        if ($this->active) {
-            $this->npv->value_gbp($time, $this->value_maintenance_per_timestep_gbp);
+    public function value($array_value, $name): float {
+        // return value as sum of array or value
+        if (isset($array_value[$name])) {
+            return is_array($array_value[$name]) ? array_sum($array_value[$name]) : $array_value[$name];
+        }
+        else {
+            return 0.0;
         }
     }
 
-    public function value($array, $name, $default = 0.0): float
-    {   // return value as element or sum of array
+    public function sum_value(&$parameter, $array, $name): void {
         if (isset($array[$name])) {
-            if (is_array($array[$name])) {
-                $value = array_sum($array[$name]);
-            } else {
-                $value = $array[$name];
+            $element = $array[$name];
+            foreach ($parameter as $k => $v) {
+                $value = $this->value($element, $k);
+                $parameter[$k] += $value;
             }
-        } else {
-            $value = $default ?: 0.0;
         }
-        return $value;
-    }
-
-    public function overlay($array_base, $array_over): array
-    {
-        foreach ($array_over as $key => $value) {
-            $array_base[$key] = $value;
-        }
-        return $array_base;
     }
 }

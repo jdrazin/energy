bdd1f9e6bf87305a1a782386f4462a53832ad93c
jdrazin <jonathan@drazin.net>
2025-07-02 09:23:56 +0100

    Corrected

--- src/log_db.php
+++ src/log_db.php
@@ -2,12 +2,11 @@
 declare(strict_types=1);
 namespace Src;
 use Exception;
-use GuzzleHttp\Exception\GuzzleException;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /*
- * initialise controller
+ * log message to db
  *
  */
 
@@ -18,48 +17,29 @@ ini_set('max_execution_time', '36000');
 ini_set('mysql.connect_timeout','36000');
 
 const FOLDER_PID                     = '/var/www/html/energy/pids/',
+      ARGS                           = ['MESSAGE' => 1],
       DEBUG                          = false;  // disable cron and semaphore single thread control
 
-try {
-    if (!DEBUG) {
-        $pid_filename = FOLDER_PID . basename(__FILE__, '.php') . '.pid';
-        if (file_exists($pid_filename)) {
-            echo 'Cannot start: semaphore exists';
-            exit(1);
-        }
-        else {
-            file_put_contents($pid_filename, getmypid());
-        }
+if (!DEBUG) {
+    $pid_filename = FOLDER_PID . basename(__FILE__, '.php') . '.pid';
+    if (file_exists($pid_filename)) {
+        echo 'Cannot start: semaphore exists';
+        exit(1);
     }
+    else {
+        file_put_contents($pid_filename, getmypid());
+    }
+}
+if ($message = trim($argv[ARGS['MESSAGE']]) && !DEBUG) {
     $root = new Root();
-    $root->logDb('MESSAGE', 'Initialising ...', null,'NOTICE');
-    (new GivEnergy())->initialise(true);              // set charge discharge blocks
-    $root->logDb('MESSAGE', 'Initialise done', null,'NOTICE');
+    $root->logDb('MESSAGE', $message, null,'NOTICE');
+}
+if (!DEBUG) {
+    if (!unlink($pid_filename)) {
+        throw new Exception('Cannot delete semaphore');
+    }
+}
+echo 'Done';
+exit(0);
 
-    if (!DEBUG) {
-        if (!unlink($pid_filename)) {
-            throw new Exception('Cannot delete semaphore');
-        }
-    }
-    echo 'Done';
-    exit(0);
-}
-catch (exception $e) {
-    $message = $e->getMessage();
-    if (EMAIL_NOTIFICATION_ON_ERROR) {
-        (new SMTPEmail())->email(['subject'  => 'EnergyController: Error',
-                                  'html'     => false,
-                                  'bodyHTML' => $message,
-                                  'bodyAlt'  => strip_tags($message)]);
-    }
-    $root = new Root();
-    $root->logDb('MESSAGE', $message,  null, 'FATAL');
-    echo $message . PHP_EOL;
-    exit(1);
-}
-catch (GuzzleException $e) {
-    $message = $e->getMessage();
-    (new Root())->logDb('MESSAGE', $message, null, 'FATAL');
-    echo $message . PHP_EOL;
-    exit(1);
-}
+

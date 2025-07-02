5cbc59c58fa38f9870568374bdc09d2fb9df17d9
jdrazin <jonathan@drazin.net>
2025-07-02 09:41:38 +0100

    Corrected

--- src/cleanup_initialise.sh
+++ src/cleanup_initialise.sh
@@ -8,9 +8,9 @@ OLD_FILES=$(find "$TARGET_DIR" -maxdepth 1 -type f -mmin +180)
 
 # if any such files exist then delete them and initialise
 if [[ -n "$OLD_FILES" ]]; then
-    echo "Files older than 3 hours found. Deleting all files in $TARGET_DIR and initialising ..."
+    php  /var/www/html/energy/src/log_db.php "Files older than 3 hours found. Deleting all files in $TARGET_DIR and initialising ..." "ERROR"
     find "$TARGET_DIR" -maxdepth 1 -type f -delete
     php  /var/www/html/energy/src/initialise.php
 else
-    echo "No files older than 3 hours: no intervention"
+    php  /var/www/html/energy/src/log_db.php "No files older than 3 hours: no intervention" "NOTICE"
 fi

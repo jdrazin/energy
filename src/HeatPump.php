1e32881744ef5d4c0fad3a1245acb936fa100d1e
jdrazin <jonathan@drazin.net>
2025-07-05 18:51:56 +0000

    lots history

--- src/HeatPump.php
+++ src/HeatPump.php
@@ -13,7 +13,7 @@ class HeatPump extends Component
         if ($this->active) {
             $this->cops = $component['cops'];
             ksort($this->cops);  // ensure cops data are in temperature order
-            $this->scop = ($component['scop'] ?? null);
+            $this->scop = $component['scop'] ?? null;
             $this->max_output_j = 1000 * ($component['output_kw'] ?? 0.0) * $this->step_s;
             $this->energy_background_step_j = ($component['power_background_w'] ?? 0.0) * $this->step_s;
             $this->heat = ($component['heat'] ?? true);

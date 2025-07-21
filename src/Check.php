0a185ed4dde4641762d3a4004b2d332e8da54fc8
jdrazin <jonathan@drazin.net>
2025-07-21 17:53:00 +0000

    NPV split into absolute and relative

--- src/Check.php
+++ src/Check.php
@@ -9,8 +9,25 @@ class Check
     const MIN = 0,
           MAX = 1;
 
-    protected function checkValue($config, $component, $parameter, $ranges) {
-        $range = $ranges[$parameter] ?? [];
+    /**
+     * @throws Exception
+     */
+    protected function checkValue($config, $component, $parameter, $parameters)
+    {
+        $checks = $parameters[$parameter] ?? [];
+        foreach ($checks as $check_type => $check_parameters) {
+            switch ($check_type) {
+                case 'range': {
+                    $this->range($config, $component, $parameter, $check_parameters);
+                    break;
+                }
+                default: {
+                }
+            }
+        }
+    }
+
+    private function range($config, $component, $parameter, $check_parameters): void {
         $string = '\'' . $component . '\' component ';
         if (!isset($config[$component])) {
             throw new Exception($string . 'is missing');
@@ -23,16 +40,15 @@ class Check
             throw new Exception($string . 'is null');
         }
         $string .= 'cannot be ';
-        if (isset($range[self::MIN])) {
-            if ($value < $range[self::MIN]) {
-                throw new Exception($string . 'less than ' . $range[self::MIN]);
+        if (isset($check_parameters[self::MIN])) {
+            if ($value < $check_parameters[self::MIN]) {
+                throw new Exception($string . 'less than ' . $check_parameters[self::MIN]);
             }
         }
-        if (isset($range[self::MAX])) {
-            if ($value > $range[self::MAX]) {
-                throw new Exception( $string . 'more than ' . $range[self::MAX]);
+        if (isset($check_parameters[self::MAX])) {
+            if ($value > $check_parameters[self::MAX]) {
+                throw new Exception( $string . 'more than ' . $check_parameters[self::MAX]);
             }
         }
-        return $value;
     }
 }

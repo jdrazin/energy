8ea79c8a9f91ce2eb4688b86ad537e17f970f627
jdrazin <jonathan@drazin.net>
2025-07-22 16:51:44 +0000

    NPV split into absolute and relative

--- src/Boiler.php
+++ src/Boiler.php
@@ -8,12 +8,26 @@ require_once __DIR__ . "/Energy.php";
 
 class Boiler extends Component
 {
+    const string COMPONENT_NAME = 'boiler';
+    const array CHECKS = [
+        'include'       => [
+                                'boolean' => null
+                           ],
+        'output_kw'     =>  [
+                                'range' => [0.0, 100.0]
+                            ],
+        'efficiency'     =>  [
+                                'range' => [0.0, 1.0]
+                            ]
+                        ];
 
     public float $efficiency, $max_output_j;
 
-    function __construct($component, $time)
+    function __construct($config, $time)
     {
-        parent::__construct($component, $time);
+        parent::__construct($config, $time);
+        $suffixes = [];
+        $this->include = $this->checkValue($config, $suffixes, self::COMPONENT_NAME, 'include', self::CHECKS);;
         if ($this->include) {
             $this->max_output_j = ($component['output_kw'] * 1000 ?? 0.0) * $this->step_s;
             $this->efficiency = $component['efficiency'] ?? 1.0;

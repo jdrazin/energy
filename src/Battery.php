8f5735b541aea75d13a25ca14ee9e891be7f89e0
jdrazin <jonathan@drazin.net>
2025-07-24 15:30:02 +0000

    NPV split into absolute and relative

--- src/Battery.php
+++ src/Battery.php
@@ -5,21 +5,31 @@ require_once __DIR__ . "/Energy.php";
 
 class Battery extends Component
 {
+    const string COMPONENT_NAME = 'battery';
+    const array CHECKS = [
+        'battery'                                   => ['array'    => null           ],
+        'include'                                   => ['boolean'  => null           ],
+        'round_trip_efficiency_percent'             => ['range'    => [0.0,     100.0]],
+        'initial_raw_capacity_kwh'                  => ['range'    => [0.0,    1000.0]],
+        'cycles_to_reduced_capacity'                => ['range'    => [0.0, 1000000.0]],
+        'reduced_capacity_percent'                  => ['range'    => [0.0,     100.0]],
+        'max_charge_kw'                             => ['range'    => [0.0,     100.0]],
+        'max_discharge_kw'                          => ['range'    => [0.0,     100.0]],
+    ];
     public float    $max_charge_w, $store_j, $one_way_efficiency, $initial_raw_capacity_kwh, $cycles_to_reduced_capacity,
                     $reduced_capacity, $capacity_kwh, $store_j_max, $max_discharge_w, $cycles;
 
-    public function __construct($component, $time)
-    {
-        parent::__construct($component, $time);
-        if ($this->include) {
-            $this->one_way_efficiency           = sqrt(($component['round_trip_efficiency_percent'] ?? 100.0) / 100.0);
-            $this->initial_raw_capacity_kwh     = $component['initial_raw_capacity_kwh'] ?? 0.0;
-            $this->cycles_to_reduced_capacity   = $component['projection']['cycles_to_reduced_capacity'] ?? 1E9;
-            $this->reduced_capacity             = 1.0 - (($component['projection']['reduced_capacity_percent'] ?? 0)/100.0);
-            $this->max_charge_w                 = 1000.0 * $component['max_charge_kw'];
-            $this->max_discharge_w              = 1000.0 * $component['max_discharge_kw'];
-            $this->store_j                      = 0.0;
+    public function __construct($check, $config, $time) {
+        if ($check->checkValue($config, self::COMPONENT_NAME, [], 'include', self::CHECKS)) {
+            parent::__construct($check, $config, self::COMPONENT_NAME, $time);
+            $this->one_way_efficiency           = sqrt($check->checkValue($config, self::COMPONENT_NAME, [], 'round_trip_efficiency_percent', self::CHECKS, 100.0)/100.0);
+            $this->initial_raw_capacity_kwh     = $check->checkValue($config, self::COMPONENT_NAME, [], 'initial_raw_capacity_kwh', self::CHECKS);
+            $this->cycles_to_reduced_capacity   = $check->checkValue($config, self::COMPONENT_NAME, [], 'initial_raw_capacity_kwh', self::CHECKS, 1E9);
+            $this->reduced_capacity             = 1.0 - ($check->checkValue($config, self::COMPONENT_NAME, [], 'reduced_capacity_percent', self::CHECKS, 0.0)/100.0);
+            $this->max_charge_w                 = 1000.0 * $check->checkValue($config, self::COMPONENT_NAME, [], 'max_charge_kw',    self::CHECKS);
+            $this->max_discharge_w              = 1000.0 * $check->checkValue($config, self::COMPONENT_NAME, [], 'max_discharge_kw', self::CHECKS);
             $this->store_j_max                  = $this->initial_raw_capacity_kwh * Energy::JOULES_PER_KWH;
+            $this->store_j                      = 0.0;
             $this->cycles                       = 0.0;
         }
     }

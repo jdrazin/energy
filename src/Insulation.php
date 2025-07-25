feef781c12081e5ea0bed6651bc36c7a76754ea5
jdrazin <jonathan@drazin.net>
2025-07-25 08:18:30 +0000

    NPV split into absolute and relative

--- src/Insulation.php
+++ src/Insulation.php
@@ -3,13 +3,19 @@ namespace Src;
 
 class Insulation extends Component
 {
-    public  $space_heating_demand_factor = 1.0;
+    const string COMPONENT_NAME = 'insulation';
+    const array CHECKS = [
+        'include'                                   => ['boolean' => null],
+        'space_heating_demand_reduction_percent'    => ['range'   => 0.0, 100.0]
+    ];
+    public float $space_heating_demand_factor = 1.0;
 
-    public function __construct($component, $time)
+    public function __construct($check, $config, $time)
     {
-        parent::__construct($component, $time);
-        if ($this->include) {
-            $this->space_heating_demand_factor = (100.0 - ($component['space_heating_demand_reduction_percent'] ?? 0.0))/100.0;
+        parent::__construct($check, $config, self::COMPONENT_NAME, $time);
+        if ($this->include = $check->checkValue($config, self::COMPONENT_NAME, [], 'include', self::CHECKS, true)) {
+            $space_heating_demand_reduction_percent = $check->checkValue($config, self::COMPONENT_NAME, [], 'space_heating_demand_reduction_percent', self::CHECKS);
+            $this->space_heating_demand_factor = (100.0 - $space_heating_demand_reduction_percent)/100.0;
         }
     }
 }

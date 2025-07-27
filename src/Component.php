f533b0e783320729bad8055204d45bbdba632d9c
jdrazin <jonathan@drazin.net>
2025-07-27 12:32:56 +0000

    NPV split into absolute and relative

--- src/Component.php
+++ src/Component.php
@@ -23,21 +23,10 @@ class Component
         $this->step_s = $time->step_s;
 
         // sum install and ongoing costs
-        $this->value_install_gbp                    = 0.0;
-        $this->value_maintenance_per_timestep_gbp   = 0.0;
-        if (isset($component->cost)) { // sum elements
-            $this->cost = [
-                'install_gbp'          => 0.0,
-                'maintenance_pa_gbp'   => 0.0,
-                'standing_gbp_per_day' => 0.0
-            ];
-            $this->sum_value($this->cost, $config, 'cost'); // sun cost components
-            $this->value_install_gbp                    = -$this->value($component['cost'], 'cost_install_gbp');
-            $value_maintenance_pa_gbp                   = -$this->value($component, 'cost_maintenance_pa_gbp');
-            $value_maintenance_pa_gbp                   -= $this->value($component, 'standing_gbp_per_day') * Energy::DAYS_PER_YEAR;
-            $this->value_maintenance_per_timestep_gbp   = $value_maintenance_pa_gbp * $this->step_s / (Energy::DAYS_PER_YEAR * Energy::HOURS_PER_DAY * Energy::SECONDS_PER_HOUR);
-            $this->value_install_gbp                    = -$this->cost['install_gbp'];
-            $this->value_maintenance_per_timestep_gbp   = -($this->cost['maintenance_pa_gbp'] + Energy::DAYS_PER_YEAR * $this->cost['standing_gbp_per_day']) * $time->step_s / (Energy::DAYS_PER_YEAR * Energy::HOURS_PER_DAY * Energy::SECONDS_PER_HOUR);
+        $this->value_install_gbp                  = 0.0;
+        $this->value_maintenance_per_timestep_gbp = 0.0;
+        if (isset($component['cost'])) { // sum elements
+            $this->accumulate_cost($this->cost, $component, 'cost'); // sun cost components
         }
     }
 
@@ -57,7 +46,7 @@ class Component
         }
     }
 
-    public function sum_value(&$parameter, $array, $name): void {
+    public function accumulate_cost(&$parameter, $array, $name): void {
         if (isset($array[$name])) {
             $element = $array[$name];
             foreach ($parameter as $k => $v) {
@@ -65,5 +54,7 @@ class Component
                 $parameter[$k] += $value;
             }
         }
+        $this->value_install_gbp                  -= $parameter['cost_install_gbp'];
+        $this->value_maintenance_per_timestep_gbp -= ($parameter['maintenance_pa_gbp'] + $parameter['standing_gbp_per_day']) * $this->step_s / (Energy::HOURS_PER_DAY * Energy::SECONDS_PER_HOUR);
     }
 }

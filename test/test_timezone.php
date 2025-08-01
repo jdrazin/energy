412e38bef3357dd0f52fdd77ed7a06639888af70
jdrazin <jonathan@drazin.net>
2025-08-01 08:29:27 +0000

    NPV split into absolute and relative

--- test/test_timezone.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $datetime = new DateTime('2025-04-07 12:34:56');
-  echo  $datetime->format('Y-m-d H:i:s') . PHP_EOL;
-  $datetime->setTimezone(new DateTimeZone('Europe/London'));
-  echo  $datetime->format('Y-m-d H:i:s') . PHP_EOL;
-

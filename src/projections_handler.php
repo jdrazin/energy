b35de4c9f3893ba4f8a3a801cdda5a0ca06146bf
jdrazin <jonathan@drazin.net>
2025-08-19 09:34:00 +0000

    New config

--- src/projections_handler.php
+++ src/projections_handler.php
@@ -12,13 +12,14 @@ ini_set('mysql.connect_timeout', '36000');
 ini_set('max_execution_time',    '36000');
 ini_set('mysql.connect_timeout', '36000');
 
-const     DEBUG                       = true,
-          FOLDER_PID                  = '/var/www/html/energy/pids/',
-          TEST_PROJECTION_ID          = null,
-          TEST_PROJECTION_PATH        = '/var/www/html/energy/test/config.json',
-          ARGS                        = ['CRON' => 1],
-          INITIALISE_ON_EXCEPTION     = true,
-          EMAIL_NOTIFICATION_ON_ERROR = false;
+const     DEBUG                     = true,
+          FOLDER_PID                = '/var/www/html/energy/pids/',
+          TEST_PROJECTION_ID        = null,
+          TEST_PROJECTION_PATH      = '/var/www/html/energy/test/config.json',
+          ARGS                      = ['CRON' => 1],
+          INITIALISE_ON_EXCEPTION   = true,
+          EMAIL_ON_COMPLETION       = false,
+          EMAIL_ON_ERROR            = false;
 
 try {
     $pid_filename = FOLDER_PID . basename(__FILE__, '.php') . '.pid';
@@ -52,7 +53,7 @@ try {
 }
 catch (exception $e) {
     $message = $e->getMessage();
-    if (EMAIL_NOTIFICATION_ON_ERROR) {
+    if (EMAIL_ON_ERROR) {
         (new SMTPEmail())->email(['subject'   => 'EnergyController: Error',
                                   'html'      => false,
                                   'bodyHTML'  => $message,

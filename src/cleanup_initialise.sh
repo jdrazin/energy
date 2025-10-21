e84337930ec8f5dc95f57ebd41014e8df66cba95
jdrazin <jonathan@drazin.net>
2025-10-21 08:14:44 +0100

    Corrected

--- src/cleanup_initialise.sh
+++ src/cleanup_initialise.sh
@@ -4,13 +4,13 @@
 TARGET_DIR="/var/www/html/energy/pids"
 
 # find files older than 3 hours
-OLD_FILES=$(find "$TARGET_DIR" -maxdepth 1 -type f -mmin +180)
+OLD_FILES=$(find "$TARGET_DIR" -maxdepth 1 -type f -mmin +45)
 
 # if any such files exist then delete them and initialise
 if [[ -n "$OLD_FILES" ]]; then
-    php  /var/www/html/energy/src/log_db.php "Files older than 3 hours found. Deleting all files in $TARGET_DIR and initialising ..." "ERROR"
+    php  /var/www/html/energy/src/log_db.php "Old semaphores! Initialising ..." "ERROR"
     find "$TARGET_DIR" -maxdepth 1 -type f -delete
     php  /var/www/html/energy/src/initialise.php
 else
-    php  /var/www/html/energy/src/log_db.php "No files older than 3 hours: no intervention" "NOTICE"
+    php  /var/www/html/energy/src/log_db.php "No old semaphores" "NOTICE"
 fi

fb5cb291cd6b02fb15ad9a5caa5ef120dd3f8315
jdrazin <jonathan@drazin.net>
2025-10-21 08:15:15 +0100

    Corrected

--- src/cleanup_initialise.sh
+++ src/cleanup_initialise.sh
@@ -8,7 +8,7 @@ OLD_FILES=$(find "$TARGET_DIR" -maxdepth 1 -type f -mmin +45)
 
 # if any such files exist then delete them and initialise
 if [[ -n "$OLD_FILES" ]]; then
-    php  /var/www/html/energy/src/log_db.php "Old semaphores! Initialising ..." "ERROR"
+    php  /var/www/html/energy/src/log_db.php "Old semaphores: initialising ..." "ERROR"
     find "$TARGET_DIR" -maxdepth 1 -type f -delete
     php  /var/www/html/energy/src/initialise.php
 else

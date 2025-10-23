c555932bf6987d4da8c157b15f301d70f08e9aff
jdrazin <jonathan@drazin.net>
2025-10-23 19:18:27 +0100

    Corrected

--- src/Octopus.php
+++ src/Octopus.php
@@ -12,16 +12,18 @@ class Octopus extends Root
                     ELECTRICITY_TARIFFS = 'electricity-tariffs/';
     const array     DIRECTIONS = [
                                    'import' => [
-                                        'tariffs'   => 'tariff_imports',
-                                        'rates'     => 'tariff_rates_import',
-                                        'slot_pers' => ['KWH' => 'import_gbp_per_kwh',
-                                                        'DAY' => 'import_gbp_per_day']
+                                        'tariffs'       => 'tariff_imports',
+                                        'rates'         => 'tariff_rates_import',
+                                        'slot_pers'     => ['KWH' => 'import_gbp_per_kwh',
+                                                            'DAY' => 'import_gbp_per_day'],
+                                       'extraordinary'  => 'import_rate'
                                     ],
                                     'export' => [
-                                        'tariffs'   => 'tariff_exports',
-                                        'rates'     => 'tariff_rates_export',
-                                        'slot_pers' => ['KWH' => 'export_gbp_per_kwh',
-                                                        'DAY' => 'export_gbp_per_day']
+                                        'tariffs'       => 'tariff_exports',
+                                        'rates'         => 'tariff_rates_export',
+                                        'slot_pers'     => ['KWH' => 'export_gbp_per_kwh',
+                                                            'DAY' => 'export_gbp_per_day'],
+                                        'extraordinary' => 'import_rate'
                                     ]
                                 ],
                     RATE_PERS = [
@@ -283,12 +285,17 @@ class Octopus extends Root
             $ratesPer = ['start' => $start, 'stop' => $stop];
             foreach (self::DIRECTIONS as $direction => $x) {
                 $tariff_table = self::DIRECTIONS[$direction]['rates'];
-                foreach (self::RATE_PERS as $unit => $y) {
-                    if (is_null($ratesPer[$unit][$direction] = $this->ratePerUnit($unit, $start, $stop, $tariff_combination[$direction], $tariff_table, 0))) {      // get rate
-                        if (is_null($ratesPer[$unit][$direction] = $this->ratePerUnit($unit, $start, $stop, $tariff_combination[$direction], $tariff_table, -1))) { // if none, try same slot in previous date
-                            throw new \Exception('no ' . $direction . ' tariff between ' . $start . ' and ' . $stop);                                                          // otherwise throw exception
+                if (is_null($rate_extraordinary = $this->rateExtraordinary(self::DIRECTIONS[$direction]['rates'], $start, $stop))) {                                             // check extraordinary rate override
+                    foreach (self::RATE_PERS as $unit => $y) {
+                        if (is_null($ratesPer[$unit][$direction] = $this->ratePerUnit($unit, $start, $stop, $tariff_combination[$direction], $tariff_table, 0))) {      // get rate
+                            if (is_null($ratesPer[$unit][$direction] = $this->ratePerUnit($unit, $start, $stop, $tariff_combination[$direction], $tariff_table, -1))) { // if none, try same slot in previous date
+                                throw new \Exception('no ' . $direction . ' tariff between ' . $start . ' and ' . $stop);                                                // otherwise throw exception
+                            }
                         }
                     }
+                }
+                else {
+                    $ratesPer['KWH'][$direction] = $rate_extraordinary;
                 }
             }
             // insert rate into slot
@@ -299,6 +306,25 @@ class Octopus extends Root
             $stmt->execute();
         }
         $this->mysqli->commit();
+    }
+
+    private function rateExtraordinary($rate, $start, $stop): float|null {
+        $sql = 'SELECT `' . $rate . '`
+                    FROM    `tariff_extraordinaries_per_kwh`
+                    WHERE   ? BETWEEN `start` AND `stop` AND
+                            ? BETWEEN `start` AND `stop` AND
+                            `status` IN(\'CURRENT\', \'TO_DROP\')
+                    ORDER BY `timestamp` DESC';
+        if (!($stmt = $this->mysqli->prepare($sql)) ||
+            !$stmt->bind_param('ss', $start, $stop) ||
+            !$stmt->bind_result($rate) ||
+            !$stmt->execute()) {
+            $message = $this->sqlErrMsg(__CLASS__, __FUNCTION__, __LINE__, $this->mysqli, $sql);
+            $this->logDb('MESSAGE', $message, null, 'ERROR');
+            throw new Exception($message);
+        }
+        $stmt->fetch();
+        return $rate;
     }
 
     /**

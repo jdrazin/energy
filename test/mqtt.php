45b464879c24a7d65de7181c341e363435026cb6
jdrazin <jonathan@drazin.net>
2025-11-02 18:23:24 +0000

    New config

--- test/mqtt.php
+++ test/mqtt.php
@@ -4,35 +4,30 @@ declare(strict_types=1);
 namespace Src;
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-require('vendor/autoload.php');
-
 use \PhpMqtt\Client\MqttClient;
 use \PhpMqtt\Client\ConnectionSettings;
 
-$server   = 'broker.emqx.io';
-$port     = 1883;
-$clientId = rand(5, 15);
-$username = 'emqx_user';
-$password = 'public';
-$clean_session = false;
-$mqtt_version = MqttClient::MQTT_3_1_1;
+$server         = '192.168.1.11';
+$port           = 1883;
+$username       = 'jdrazin';
+$password       = 'mn1CjX7lWG018hP47qkv';
+$clean_session  = false;
+$mqtt_version   = MqttClient::MQTT_3_1_1;
 
-$connectionSettings = (new ConnectionSettings)
-    ->setUsername($username)
-    ->setPassword($password)
-    ->setKeepAliveInterval(60)
-    ->setLastWillTopic('emqx/test/last-will')
-    ->setLastWillMessage('client disconnect')
-    ->setLastWillQualityOfService(1);
+$connectionSettings = (new ConnectionSettings)  ->setUsername($username)
+                                                ->setPassword($password)
+                                                ->setKeepAliveInterval(60)
+                                                ->setLastWillTopic('emqx/test/last-will')
+                                                ->setLastWillMessage('client disconnect')
+                                                ->setLastWillQualityOfService(1);
 
 
-$mqtt = new MqttClient($server, $port, $clientId, $mqtt_version);
+$mqtt = new MqttClient($server, $port, null, $mqtt_version);
 
 $mqtt->connect($connectionSettings, $clean_session);
 printf("client connected\n");
 
-$mqtt->subscribe('emqx/test', function ($topic, $message) {
+$mqtt->subscribe('abc/test', function ($topic, $message) {
     printf("Received message on topic [%s]: %s\n", $topic, $message);
 }, 0);
 

7e79d39e82510d2d90ed5b2d9ee0ed9627ede243
jdrazin <jonathan@drazin.net>
2025-11-02 21:50:20 +0000

    New config

--- test/mqtt.php
+++ test/mqtt.php
@@ -1,11 +1,12 @@
 <?php
-
 declare(strict_types=1);
 namespace Src;
-require_once __DIR__ . '/../vendor/autoload.php';
 
+use Energy\src\Mqtt;
 use \PhpMqtt\Client\MqttClient;
 use \PhpMqtt\Client\ConnectionSettings;
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $server         = '192.168.1.11';
 $port           = 1883;
@@ -13,11 +14,14 @@ $username       = 'jdrazin';
 $password       = 'mn1CjX7lWG018hP47qkv';
 $clean_session  = false;
 $mqtt_version   = MqttClient::MQTT_3_1_1;
+$topic_base     = ['test', 'abc'];
+
+$mqtt           = new Mqtt($topic_base);
 
 $connectionSettings = (new ConnectionSettings)  ->setUsername($username)
                                                 ->setPassword($password)
                                                 ->setKeepAliveInterval(60)
-                                                ->setLastWillTopic('emqx/test/last-will')
+                                                ->setLastWillTopic('abc/test/last-will')
                                                 ->setLastWillMessage('client disconnect')
                                                 ->setLastWillQualityOfService(1);
 
@@ -37,16 +41,11 @@ for ($i = 0; $i< 10; $i++) {
         'date' => date('Y-m-d H:i:s'),
         'url' => 'https://github.com/emqx/MQTT-Client-Examples'
     );
-    $mqtt->publish(
-    // topic
-        'emqx/test',
-        // payload
-        json_encode($payload),
-        // qos
-        0,
-        // retain
-        true
-    );
+    $mqtt->publish('abc/test', // topic
+                    json_encode($payload),       // payload
+                    0,        // qos
+               true        // retain
+                    );
     printf("msg $i send\n");
     sleep(1);
 }

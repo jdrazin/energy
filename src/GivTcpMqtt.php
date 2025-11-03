3759b82403444fea03c23c442a699c62c4450922
jdrazin <jonathan@drazin.net>
2025-11-03 11:07:43 +0000

    New config

--- src/GivTcpMqtt.php
+++ src/GivTcpMqtt.php
@@ -17,12 +17,12 @@ require_once __DIR__ . "/Energy.php";
  * GivEnergy wrapper class for MQTT
  *
  * see:
- *   - https://github.com/php-mqtt/client for GivTCPMqtt stack
+ *   - https://github.com/php-mqtt/client for GivTcpMqtt stack
  *   - https://github.com/britkat1980/giv_tcp/blob/main/README.md for GivTCP
  *
  */
 
-class GivTCPMqtt {
+class GivTcpMqtt {
     const bool      CLEAN_SESSION         = false;
 
     const int       KEEP_ALIVE_SECONDS    = 60,
